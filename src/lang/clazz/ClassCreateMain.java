960c4916037da89a6ff80dcdc556c073a7f416d0
JunBe <wnsqowid0901@gmail.com>
2024-07-21 19:46:50 +0900

    class

---
+++ src/lang/clazz/ClassCreateMain.java
@@ -0,0 +1,11 @@
+package lang.clazz;
+
+public class ClassCreateMain {
+    public static void main(String[] args) throws Exception{
+        Class helloClass = Class.forName("lang.clazz.Hello");
+        Hello hello = (Hello) helloClass.getDeclaredConstructor().newInstance();
+        System.out.println("hello = " + hello);
+        String result = hello.hello();
+        System.out.println("result = " + result);
+    }
+}
