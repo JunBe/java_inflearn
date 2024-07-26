f82d226c609e39d7a143195faebdcc0a15606ac5
JunBe <wnsqowid0901@gmail.com>
2024-07-27 02:16:03 +0900

    내부 클래스

--- src/nested/inner/ex1/Car.java
+++ src/nested/inner/ex1/Car.java
@@ -1,4 +1,4 @@
-package nested.nested.inner.ex2;
+package nested.inner.ex1;
 
 public class Car {
     private String model;
@@ -8,20 +8,19 @@ public class Car {
     public Car(String model, int chargeLevel) {
         this.model = model;
         this.chargeLevel = chargeLevel;
-        this.engine = new Engine();
+        this.engine = new Engine(this);
     }
 
+    public int getChargeLevel() {
+        return chargeLevel;
+    }
+
+    public String getModel() {
+        return model;
+    }
 
     public void start() {
         engine.start();
         System.out.println(model + " 시작 완료");
-    }
-
-    private class Engine{
-        public void start() {
-            System.out.println("충전 레벨 확인: " + chargeLevel);
-            System.out.println(model + "의 엔진을 구동합니다.");
-        }
-
     }
 }
--- src/nested/inner/ex2/Car.java
+++ src/nested/inner/ex2/Car.java
@@ -1,4 +1,4 @@
-package nested.nested.inner.ex1;
+package nested.inner.ex2;
 
 public class Car {
     private String model;
@@ -8,19 +8,20 @@ public class Car {
     public Car(String model, int chargeLevel) {
         this.model = model;
         this.chargeLevel = chargeLevel;
-        this.engine = new Engine(this);
+        this.engine = new Engine();
     }
 
-    public int getChargeLevel() {
-        return chargeLevel;
-    }
-
-    public String getModel() {
-        return model;
-    }
 
     public void start() {
         engine.start();
         System.out.println(model + " 시작 완료");
+    }
+
+    private class Engine{
+        public void start() {
+            System.out.println("충전 레벨 확인: " + chargeLevel);
+            System.out.println(model + "의 엔진을 구동합니다.");
+        }
+
     }
 }
